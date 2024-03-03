d42e85060363e89d212185179362cdd59178f5fc
Droopytex <a.velasquezvega@gmail.com>
2024-03-03 01:32:19 -0300

    ajustes a  codigo js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
-precio = 400000;
+const precio = 400000;
+const cantidad = 0;
+const precioInicial = 0;
+
+const valorTotal = precio * cantidad;
 
 precioSpan = document.querySelector(".precio-inicial");
-
 precioSpan.innerHTML = precio;
+
+precioInicialSpan = document.querySelector(".cantidad");
+
+cantidadSpan = document.querySelector(".cantidad");
+
+valorTotalSpan = document.querySelector(".valor-total");
